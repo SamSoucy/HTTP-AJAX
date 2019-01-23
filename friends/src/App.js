71090faee9b9ea6e428f2fe9199a03d58c820def
Samsoucy <newf930@outlook.com>
2019-01-23 17:16:10 -0500

    made change to server file

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: [],
+      error: ""
+    };
+}
+
+componentDidMout() {
+  axios
+    .get("http://localhost:5000/friends")
+    .then(res => {
+      this.setState({
+        friends: res.data,
+        error: ""
+      });
+    })
+    .catch(err => {
+      this.setState({ error: err });
+});
+    
+}
+
   render() {
     return (
       <div className="App">
-       
-      </div>
+                
+        {this.state.error && <h4>{this.state.error}</h4>}
+
+        {this.state.friends.map(friend =>
+          <div>
+            <h1>{friend.name}</h1>
+            <p><span>Age:</span> {friend.age}</p>
+            <p><span>Email:</span> {friend.email}</p>
+          </div>)}
+
+       </div>
     );
   }
 }
